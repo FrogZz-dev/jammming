fddc876d22adece2c598ebda8a7671cfa3b66249
FrogZz-dev <71564463+FrogZz-dev@users.noreply.github.com>
2020-10-27 19:40:37 +0100

    Updates

---
+++ precache-manifest.6763219a30279d18fafe0157efe2f253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0950432e5412da7b573711415116376e",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "56a954184c812154ad9e",
+    "url": "/jammming/static/css/main.67554516.chunk.css"
+  },
+  {
+    "revision": "6a65db149cd536a0250a",
+    "url": "/jammming/static/js/2.4b055878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.4b055878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a954184c812154ad9e",
+    "url": "/jammming/static/js/main.ec3f2a4c.chunk.js"
+  },
+  {
+    "revision": "d54c33d011ceab8fe7ea",
+    "url": "/jammming/static/js/runtime-main.af6152f4.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
